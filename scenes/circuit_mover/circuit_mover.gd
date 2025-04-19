9fc0147b77040cf3a82a27074fd8fc4bbad6ed2f
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2025-04-19 13:22:04 +0100

    ADD: speed threshold directionality

--- scenes/circuit_mover/circuit_mover.gd
+++ scenes/circuit_mover/circuit_mover.gd
@@ -20,13 +20,20 @@ func _process(_delta: float) -> void:
 	if race_manager.current_track_card.speed_threshold == 0:
 		return
 
-	if (path_follow_2d.progress_ratio > race_manager.current_track_card.speed_min_limit_1 and path_follow_2d.progress_ratio < race_manager.current_track_card.speed_max_limit_1) or (path_follow_2d.progress_ratio > race_manager.current_track_card.speed_min_limit_2 and path_follow_2d.progress_ratio < race_manager.current_track_card.speed_max_limit_2):
-		if speed >= race_manager.current_track_card.speed_threshold:
-			print("SPIN!")
-	elif (path_follow_2d.progress_ratio > race_manager.current_track_card.caution_min_limit_1 and path_follow_2d.progress_ratio < race_manager.current_track_card.caution_max_limit_1) or (path_follow_2d.progress_ratio > race_manager.current_track_card.caution_min_limit_2 and path_follow_2d.progress_ratio < race_manager.current_track_card.caution_max_limit_2):
-		if speed >= race_manager.current_track_card.speed_threshold:
-			print("Caution")
-
+	if race_manager.current_track_card.first_link == race_manager.current_track_card.circuit_link_1:
+		if (path_follow_2d.progress_ratio > race_manager.current_track_card.speed_min_limit_1 and path_follow_2d.progress_ratio < race_manager.current_track_card.speed_max_limit_1) or (path_follow_2d.progress_ratio > race_manager.current_track_card.speed_min_limit_2 and path_follow_2d.progress_ratio < race_manager.current_track_card.speed_max_limit_2):
+			if speed >= race_manager.current_track_card.speed_threshold:
+				print("SPIN!")
+		elif (path_follow_2d.progress_ratio > race_manager.current_track_card.caution_min_limit_1 and path_follow_2d.progress_ratio < race_manager.current_track_card.caution_max_limit_1) or (path_follow_2d.progress_ratio > race_manager.current_track_card.caution_min_limit_2 and path_follow_2d.progress_ratio < race_manager.current_track_card.caution_max_limit_2):
+			if speed >= race_manager.current_track_card.speed_threshold:
+				print("Caution")
+	else:
+		if (path_follow_2d.progress_ratio < 1 - race_manager.current_track_card.speed_min_limit_1 and path_follow_2d.progress_ratio > 1 - race_manager.current_track_card.speed_max_limit_1) or (path_follow_2d.progress_ratio < 1 - race_manager.current_track_card.speed_min_limit_2 and path_follow_2d.progress_ratio > 1 - race_manager.current_track_card.speed_max_limit_2):
+			if speed >= race_manager.current_track_card.speed_threshold:
+				print("SPIN!")
+		elif (path_follow_2d.progress_ratio < 1 - race_manager.current_track_card.caution_min_limit_1 and path_follow_2d.progress_ratio > 1 - race_manager.current_track_card.caution_max_limit_1) or (path_follow_2d.progress_ratio < 1 - race_manager.current_track_card.caution_min_limit_2 and path_follow_2d.progress_ratio > 1 - race_manager.current_track_card.caution_max_limit_2):
+			if speed >= race_manager.current_track_card.speed_threshold:
+				print("Caution")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("test_player_accelerate"):
